88a34768a11cac6f510f3ba91c5aee1cf9b659f3
rberduo <berduoronald@gmail.com>
2021-04-24 02:59:40 -0600

    # Configuracion inicial del proyecto

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import "module-alias/register";
+import "@shared/infrastructure/http/app";
---
+++ src/shared/infrastructure/http/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import { v1Router } from "./api";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(compression());
+app.use(helmet());
+app.use(morgan("combined"));
+
+app.use("/api/v1", v1Router);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`[App]: Listening on port ${port}`);
+});
